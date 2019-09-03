d83a37231ae0c085f20f1d930621d208b00dc1c8
Liam Sutton <devliam90@gmail.com>
2019-09-03 12:21:40 +0100

    add mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const h2El = document.querySelectorAll('h2');
+
+h2El.forEach(item => item.addEventListener('mouseover', (e) => {
+  item.style.cursor = 'pointer';
+  if (item.style.color != 'red') {
+    item.style.color = 'red';
+  } else {
+    item.style.color = 'black';
+  }
+}))
