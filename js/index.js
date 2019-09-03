864cc10d9a42951e539a4a5af585b9e469982cb1
Liam Sutton <devliam90@gmail.com>
2019-09-03 12:37:59 +0100

    add wheel event

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+// Add red text color to all h2's when mousing over the h2 elements
 const h2El = document.querySelectorAll('h2');
 
 h2El.forEach(item => item.addEventListener('mouseover', (e) => {
@@ -10,8 +12,24 @@ h2El.forEach(item => item.addEventListener('mouseover', (e) => {
   }
 }))
 
+// Log the key that was pressed in the console only if A-Z
 window.addEventListener('keydown', (e) => {
   let code = e.keyCode;
   if (code > 90 || code < 65) return
   console.log(`You pressed the ${String.fromCharCode(code)} key.`)
 })
+
+// Add wheel event to images
+const paragraphEl = document.querySelector('p');
+let scale = 1;
+
+paragraphEl.addEventListener('wheel', zoom);
+
+function zoom(e) {
+  e.preventDefault();
+  scale += e.deltaY * -0.01;
+  // Restrict scale
+  scale = Math.min(Math.max(.125, scale), 4);
+  // Apply scale transform
+  paragraphEl.style.transform = `scale(${scale})`;
+}
